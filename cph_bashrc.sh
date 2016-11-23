dd73796640aecf139e201c11b9e71f6acc875553
cph <stegben.benjamin@gmail.com>
2016-11-23 10:02:35 +0800

    add more git alias

--- cph_bashrc.sh
+++ cph_bashrc.sh
@@ -1,2 +1,13 @@
-alias gst='git status'
-alias gco='git checkout'
+alias gst='git status '
+alias gcm='git commit -m '
+alias gco='git checkout '
+alias gcob='git checkout -b '
+alias ga='git add'
+alias gaa='git add -A'
+alias gr='git rebase'
+alias gri='git rebase -i'
+
+function grih() {
+  git rebase -i HEAD~$1
+}
+alias grih=grih
